0157a4ec2fcf570a6323ff220a7a6d81862ab20c
Juanantonio21 <jga1404.iescc@gmail.com>
2022-12-19 18:18:08 +0100

    Aplicacion acaba

--- app/src/main/java/dam/cazorla/ejemplo1/MainActivity.java
+++ app/src/main/java/dam/cazorla/ejemplo1/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /*Todos los activities heredan de la clase Activity, pero por compatibilidad todas heredan de AppCompatActivity
 que a su vez hereda de Activity*/
@@ -23,18 +24,22 @@ public class MainActivity extends AppCompatActivity {
         iniciar = findViewById(R.id.button);
         txt1= findViewById(R.id.editText);
         txt2=findViewById(R.id.pass);
+        final Toast t= Toast.makeText(this,"Las ccrendenciales son admin admin",Toast.LENGTH_LONG);
         iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String texto = txt1.getText().toString();
                 String texto2=txt2.getText().toString();
-                if(texto2.equals("admin")||texto.equals("admin")){
+                if(texto2.equals("admin")&&texto.equals("admin")){
                 Intent intent = new Intent (view.getContext(), Main2Activity.class);
                 startActivityForResult(intent, 0);
-            }
+            }else{
+                    t.show();
+                }
 
 
             }
+
         });
 
 
